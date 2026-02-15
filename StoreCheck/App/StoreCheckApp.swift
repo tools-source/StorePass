c2e5588b7c23e7833e23a713156b9003956c132e
Majdi Nagi <majdinagi@gmail.com>
2026-02-14 22:26:48 -0500

    Create runnable StoreCheck iOS app project and shared scheme

--- StoreCheck/App/StoreCheckApp.swift
+++ StoreCheck/App/StoreCheckApp.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 @main
 struct StoreCheckApp: App {
-    (AppDelegate.self) private var appDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     @StateObject private var appContainer = AppContainer.shared
 
     var body: some Scene {
